d0cc225bf9c3490cddbeaf4b60a14d8b24e61532
Alexis Darnat <alexis.darnat@epitech.eu>
2017-09-26 16:57:21 -0700

    Add support view new fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
 	attr_accessor :login
 
 	validates :username, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A[a-zA-Z0-9 _\.]*\z/}
+  validates :country_code, length: {is: 2}, format: { with: /\A[A-Z]+\z/ }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -13,7 +14,7 @@ class User < ApplicationRecord
     def self.find_first_by_auth_conditions(warden_conditions)
     	conditions = warden_conditions.dup
     	if login = conditions.delete(:login)
-    		where(conditions.to_h).where("lower(email) = :value OR lower(:username) = :value", value: login.downcase).first
+    		where(conditions.to_h).where("lower(email) = :value OR lower(username) = :value", value: login.downcase).first
     	else
     		where(conditions.to_h).first
     	end
